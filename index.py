9c1ea786b71357b832726eaacb7cc4b3215c1a9f
Beesama <gbozee@gmail.com>
2019-12-06 15:19:09 +0100

    use a python package

--- index.py
+++ index.py
@@ -1,28 +1,3 @@
-# import json
-# import bottle
-# from bottle import response
+from sub_app.views import app 
 
-# application = bottle.Bottle()
-
-# @application.get('/')
-# def home():
-#     return {'hello': 'world'}
-
-# @application.get("/hello")
-# def hello():
-#     return {'new-hello':'world'}
-
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-
-application = Starlette()
-
-@application.route('/')
-async def homepage(request):
-    return JSONResponse({'hello': 'world'})
-
-
-@application.route("/post-call", methods=['POST'])
-async def postCall(request):
-    json = await request.json()
-    return JSONResponse({**json,"now":True})
+application = app
